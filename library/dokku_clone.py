66f76bf6ecce72c886e4bd3e13f77610043a3dea
Leopold Talirz <leopold.talirz@gmail.com>
2021-05-06 12:09:07 +0200

    add shebang

--- library/dokku_clone.py
+++ library/dokku_clone.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-# from ansible.module_utils.basic import *
 from ansible.module_utils.basic import AnsibleModule
 from ansible.module_utils.dokku_app_utils import (
     dokku_app_present,
